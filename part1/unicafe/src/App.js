7e0bf9f806c02fe96e53690158eaec3903d80c29
Juan Sóto <juansheba97@gmail.com>
2023-08-22 21:47:30 +0100

    submit part1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
 import Statistics from './Statistics';
 
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setNeutral(neutral + 1);
-  const handleBadClick = () => setBad(bad + 1);
-
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={handleGoodClick}>good</button>
-      <button onClick={handleNeutralClick}>neutral</button>
-      <button onClick={handleBadClick}>bad</button>
-
+      <Button text="good" onClick={() => setGood(good + 1)} />
+      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" onClick={() => setBad(bad + 1)} />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
-}
+};
 
 export default App;
--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,32 +1,41 @@
 import React from "react";
 
+
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>{text} {value}</p>
+  );
+};
+
+
 const Statistics = ({ good, neutral, bad }) => {
-    const total = good + neutral + bad;
-    
-    if (total === 0) {
-      return (
-        <div>
-          <h2>Statistics</h2>
-          <p>No Given Feedback</p>
-        </div>
-      );
-    }
-  
-    const average = (good - bad) / total;
-    const positivePercentage = (good / total) * 100;
-  
+  const total = good + neutral + bad;
+
+  if (total === 0) {
     return (
       <div>
         <h2>Statistics</h2>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>total {total}</p>
-        <p>average {average}</p>
-        <p>positive {positivePercentage} %</p>
+        <p>No Given Feedback</p>
       </div>
     );
   }
-  
+
+  const average = (good - bad) / total;
+  const positivePercentage = (good / total) * 100;
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="total" value={total} />
+      <StatisticLine text="average" value={average.toFixed(2)} />
+      <StatisticLine text="positive" value={positivePercentage.toFixed(2) + " %"} />
+    </div>
+  );
+};
+
 
 export default Statistics;
