cbf4f00276c4c4b0d0bf789779df97f4964677cc
Juan Sóto <juansheba97@gmail.com>
2023-08-24 23:21:14 +0100

    submit part2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,36 +1,6 @@
+import React from 'react';
 import './App.css';
-
-const Header = ({ courseName }) => {
-  return <h1>{courseName}</h1>
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <div>
-      <Header courseName={course.name} />
-      <Content parts={course.parts} />
-      <p><strong>Total of {totalExercises} exercises</strong></p>
-    </div>
-  )
-}
+import Course from './components/course.js';
 
 const App = () => {
   const courses = [
