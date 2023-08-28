ad07f15d38a2bec8e84bc79f933fbf16799570dc
Juan Sóto <juansheba97@gmail.com>
2023-08-28 22:11:00 +0100

    submit part2.13 and 2.14

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const CountryDetail = ({ country }) => {
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        if (country) {
+            const apiKey = process.env.REACT_APP_API_KEY; 
+            const query = country.capital;
+            
+            axios.get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${query}`)
+                .then(response => {
+                    setWeather(response.data.current);
+                });
+        }
+    }, [country]);
+
     if (!country) return null;
 
     return (
@@ -13,6 +28,16 @@ const CountryDetail = ({ country }) => {
                 {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
             </ul>
             <img src={country.flags.png} alt={`Flag of ${country.name}`} width="150" />
+            
+            {/* Mostrando el clima si está disponible */}
+            {weather && (
+                <div>
+                    <h3>Weather in {country.capital}</h3>
+                    <p><strong>Temperature:</strong> {weather.temperature}°C</p>
+                    <img src={weather.weather_icons[0]} alt={weather.weather_descriptions[0]} />
+                    <p><strong>Wind:</strong> {weather.wind_speed} km/h direction {weather.wind_dir}</p>
+                </div>
+            )}
         </div>
     );
 }
