b6222bb0cb2cf1e7ebd6b08a5cf79e4393dbbbc4
Juan Sóto <juansheba97@gmail.com>
2023-09-17 16:24:35 +0100

    Submit part 3.2 t 3.6

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
+
 
 let persons = [
     {
@@ -27,6 +29,64 @@ let persons = [
   app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
+
+  app.get('/info', (req, res) => {
+    const date = new Date()
+    const content = `
+        <h1>fullstack content</h1>
+        <p>${date}</p>
+        <p>The Phonebook has info for ${persons.length} persons</p>
+    `;
+
+    res.send(content);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id) // Convertir el ID a número ya que viene como string
+  const person = persons.find(p => p.id === id)
+
+  if (person) {  // Si se encuentra la persona, se devuelve.
+      res.json(person)
+  } else {  // Si no se encuentra, se devuelve un error 404.
+      res.status(404).send('<h2>Persona no encontrada</h2>')
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id); // Convertir el ID a número ya que viene como string
+  persons = persons.filter(p => p.id !== id); // Filtra y elimina la persona con el ID especificado
+
+  res.status(204).end(); // 204 No Content: indica que la operación fue exitosa pero no se está enviando contenido en la respuesta
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+      return res.status(400).json({ 
+          error: 'name or number missing' 
+      });
+  }
+
+  const personExists = persons.some(person => person.name === body.name);
+  if (personExists) {
+      return res.status(400).json({ 
+          error: 'name must be unique' 
+      });
+  }
+
+  const newPerson = {
+      id: Math.floor(Math.random() * 1000000), 
+      name: body.name,
+      number: body.number
+  };
+
+  persons.push(newPerson);
+
+  res.json(newPerson);
+});
+
+
   
   const PORT = 3001
   app.listen(PORT, () => {
